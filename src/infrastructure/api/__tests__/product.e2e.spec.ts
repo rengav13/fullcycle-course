2e28c444071c11500137ec1952e141bc251885b5
Vagner Luciano Da Costa Silva <vagner.silva@volvocars.com>
2023-09-01 18:38:25 +0200

    feat: add product routes

---
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -0,0 +1,61 @@
+import request from "supertest";
+import { app, sequelize } from "../express";
+
+describe("E2E test for product", () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const response = await request(app)
+      .post("/product")
+      .send({
+        name: "Bola de gude",
+        price: 100,
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe("Bola de gude");
+    expect(response.body.price).toBe(100);
+  });
+
+  it("should not create a product", async () => {
+    const response = await request(app).post("/product").send({
+      name: "Bola de futebol",
+    });
+    expect(response.status).toBe(500);
+  });
+
+  it("should list all products", async () => {
+    const response = await request(app)
+      .post("/product")
+      .send({
+        name: "Bola de gude",
+        price: 100,
+      });
+    expect(response.status).toBe(200);
+
+    const response2 = await request(app)
+      .post("/product")
+      .send({
+        name: "Bola de futebol",
+        price: 200
+      });
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get("/product").send();
+
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.products.length).toBe(2);
+
+    expect(listResponse.body.products[0].name).toBe("Bola de gude");
+    expect(listResponse.body.products[0].price).toBe(100);
+
+    expect(listResponse.body.products[1].name).toBe("Bola de futebol");
+    expect(listResponse.body.products[1].price).toBe(200);
+  });
+});
